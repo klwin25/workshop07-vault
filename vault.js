a6d1ca079c306d0739d2ffe5bd7c0eb69f6dfff2
Kim <kimnguyentulane@gmail.com>
2024-07-22 12:34:04 -0500

    Added workshop07-vault  files

---
+++ vault.js
@@ -0,0 +1,11 @@
+// Will state the sentence in quotes below
+const str =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+//   These are the three mathematical calculations to generate the three codes in the combination to the vault
+let num1 = 9 + 1;
+let num2 = 10 * 4;
+let num3 = 40 - 1;
+
+// will show an alert to state both the string sentence above the combination for the vault
+alert(`${str} is ${num1} - ${num2} - ${num3}`);
